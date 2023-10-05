b13f8aefaa4edc06f0cd708599f8d59cebab591f
NomarCestero <147081459+NomarCestero@users.noreply.github.com>
2023-10-05 11:35:52 -0400

    Add files via upload

---
+++ Powershell IF menu.ps1
@@ -0,0 +1,13 @@
+﻿Write-Host "1-Ver info del CPU"
+Write-Host "2-Ver info del Motherboard"
+Write-Host "3-Ver info de RAM"
+$Sel = Read-Host "Seleccion:"
+if ($Sel -eq 1) {
+(Get-WmiObject Win32_Processor).Name
+}
+if ($Sel -eq 2) {
+Get-WmiObject Win32_BaseBoard
+}
+if ($Sel -eq 3) {
+Get-WmiObject win32_physicalmemory | Format-Table
+}
